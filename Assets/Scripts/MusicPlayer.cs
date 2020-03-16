2631a88bb4b6197355573da934e433505fa82ce0
David <dameny87@gmail.com>
2020-03-16 02:01:29 -0700

    Clean up Music Player

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -13,14 +13,4 @@ public class MusicPlayer : MonoBehaviour {
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
